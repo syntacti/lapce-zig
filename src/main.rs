1a8d0e06aa0085a4f27814cf184d4d9943fc74c1
Suraj Ghimire <suraj@ghishadow.com>
2022-07-18 19:22:37 +0530

    use system lsp
    Signed-off-by: Suraj Ghimire <suraj@ghishadow.com>

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
-
 use lapce_plugin::{register_plugin, start_lsp, LapcePlugin};
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
-use std::env;
 
 #[derive(Default)]
 struct State {}
@@ -17,6 +15,7 @@ pub struct PluginInfo {
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Configuration {
     language_id: String,
+    system_lsp: bool,
     options: Option<Value>,
 }
 
@@ -38,21 +37,12 @@ impl LapcePlugin for State {
             _ => return,
         };
 
-        // lapce-rust does this; is there any handler?
-        // let lock_file = PathBuf::from("download.lock");
-        // send_notification(
-        //     "python",
-        //     &json!({
-        //         "path": &lock_file,
-        //     }),
-        // );
-
-        let zls_path = match env::var("ZLS_PATH") {
-            Ok(var) => var,
-            Err(error) => panic!("Couldn't get PYLS_PATH: {error}"),
-        };
-
         serde_json::to_writer_pretty(std::io::stderr(), &info).unwrap();
-            start_lsp(&zls_path, "zig", info.configuration.options);
+        start_lsp(
+            "zls",
+            "zig",
+            info.configuration.options,
+            info.configuration.system_lsp,
+        );
     }
 }
