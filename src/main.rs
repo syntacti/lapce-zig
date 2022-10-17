98abcfa7cdc74b36dc55810c0bde2b4258775842
Suraj Ghimire <suraj@ghishadow.com>
2022-10-17 23:57:36 +0530

    implement download_zls
    Signed-off-by: Suraj Ghimire <suraj@ghishadow.com>

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
+use std::{path::Path, fs};
+
 use anyhow::Result;
 use lapce_plugin::{
     psp_types::{
         lsp_types::{request::Initialize, DocumentFilter, DocumentSelector, InitializeParams, Url},
         Request,
     },
-    register_plugin, LapcePlugin, PLUGIN_RPC,
+    register_plugin, LapcePlugin, PLUGIN_RPC, Http,
 };
 use serde_json::Value;
 
@@ -14,12 +16,14 @@ struct State {}
 register_plugin!(State);
 
 fn initialize(params: InitializeParams) -> Result<()> {
+    PLUGIN_RPC.stderr("lapce-zig");
+    download_zls()?;
     let document_selector: DocumentSelector = vec![DocumentFilter {
         language: Some(String::from("zig")),
         pattern: Some(String::from("**.zig")),
         scheme: None,
     }];
-    let mut server_args = vec!["--enable-debug-log".to_string()];
+    let mut server_args = vec![];
 
     if let Some(options) = params.initialization_options.as_ref() {
         if let Some(lsp) = options.get("lsp") {
@@ -53,8 +57,13 @@ fn initialize(params: InitializeParams) -> Result<()> {
         }
     }
 
-    let server_path = Url::parse("urn:zls")?;
+    // let server_path = Url::parse("urn:zls")?;
 
+    let volt_uri = std::env::var("VOLT_URI")?;
+    let server_path = Url::parse(&volt_uri)
+        .unwrap()
+        .join("zls")
+        .unwrap();
     PLUGIN_RPC.start_lsp(
         server_path,
         server_args,
@@ -78,6 +87,40 @@ impl LapcePlugin for State {
             _ => {}
         }
     }
+}
+
+fn download_zls() -> Result<bool> {
+    const DOWNLOADS_ROOT: &str = "https://zig.pm/zls/downloads";
+    let arch = match std::env::var("VOLT_ARCH").as_deref() {
+        Ok("x86_64") => "x86_x64",
+        Ok("aarch64") => "aarch64",
+        _ => panic!("unknow arch"),
+    };
+    let os = match std::env::var("VOLT_OS").as_deref() {
+        Ok("linux") => "linux",
+        Ok("macos") => "macos",
+        Ok("windows") => "windows",
+        _ => panic!("unknow os"),
+    };
+    
+    let lapce_zls_base_name = format!("zls");
+    let lapce_zls_path = Path::new(&lapce_zls_base_name);
+
+    if !lapce_zls_path.exists() {
+        let volt_download_url = format!(
+            "{}/{}-{}/bin/zls",
+            &DOWNLOADS_ROOT, &arch,&os
+        );
+        PLUGIN_RPC.stderr(&format!("Download_URL {}", volt_download_url));
+
+        let mut resp = Http::get(&volt_download_url)?;
+        let body = resp.body_read_all()?;
+        fs::write(&lapce_zls_path, body)?;
+    } else {
+        PLUGIN_RPC.stderr("zls already exists");
+    }
+
+    Ok(true)
 }
 
 // const LANGUAGE_ID: &str = "zig";
